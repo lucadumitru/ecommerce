df3335758d9779f457fde960cc6abd7bd6ef8c9c
lucadumitru <luca.dumitru25@gmail.com>
2024-01-22 11:35:54 +0100

    force dynamic home page products fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
+import { notFound } from "next/navigation";
+
 import { NewProductsSection } from "@/components/sections";
 import { Container } from "@/components/ui";
 import { gql } from "@/graphql/client";
 
-export const revalidate = 600;
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 const Home = async () => {
-  const products = (await gql.getProducts()).products.data;
+  const products = (await gql.getProducts().catch(notFound)).products.data;
+
+  if (!products) return notFound();
+
   return (
     <main className="grow">
       <Container>
